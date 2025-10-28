e49419c478393ed996f2e0a25bb4f0d8f82a86e0
jayashri-jadhav <jsahebrao329@gmail.com>
2025-10-28 12:05:10 +0530

    Time: 4 ms (100.00%) | Memory: 44.7 MB (87.44%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.java
@@ -0,0 +1,135 @@
+class MyCircularDeque {
+ int []arr;
+ int front;
+ int rear;
+ int size;
+ int capacity;
+    public MyCircularDeque(int k) {
+        this.capacity=k;
+        arr=new int[k];
+        size=0;
+        front=-1;
+        rear=-1;
+      
+        
+    }
+    
+    public boolean insertFront(int value) {
+        if(isFull())
+        {
+            return false;
+        }
+        if(isEmpty())
+        {
+            front=0;
+            rear=0;
+        }
+        else
+        {
+        front=(front-1+capacity)%capacity;
+        
+        }
+        arr[front]=value;
+        size++;
+        return true;
+        
+    }
+    
+    public boolean insertLast(int value) {
+        if(isFull())
+        {
+            return false;
+        }
+        if(isEmpty())
+        {
+            front=0;
+            rear=0;
+        }
+        else
+        {
+            rear=(rear+1)%capacity;
+        }
+        arr[rear]=value;
+        size++;
+        return true;
+        
+    }
+    
+    public boolean deleteFront() {
+        if(isEmpty())
+        {
+            return false;
+        }
+        if(front==rear)
+        {
+            front=-1;
+            rear=-1;
+        }
+        else
+        {
+            front=(front+1)%capacity;
+        }
+        size--;
+        return true;
+        
+    }
+    
+    public boolean deleteLast() {
+        if(isEmpty())
+        {
+            return false;
+        }
+        if(front==rear)
+        {
+            front=-1;rear=-1;
+        }
+        else
+        {
+            rear=(rear-1+capacity)%capacity;
+        }
+        size--;
+        return true;
+    }
+    
+    public int getFront() {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        return arr[front];
+        
+    }
+    
+    public int getRear() {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        else
+        {
+      
+        return arr[rear];
+        }
+    }
+    
+    public boolean isEmpty() {
+        return size==0;
+    }
+    
+    public boolean isFull() {
+        return size==capacity;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
