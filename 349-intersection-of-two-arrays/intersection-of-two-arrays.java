193da3652a745a5a8ea37e505614f8f96f053192
jayashri-jadhav <jsahebrao329@gmail.com>
2026-01-05 17:52:44 +0530

    Time: 7 ms (17.80%) | Memory: 45.2 MB (24.64%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,39 +1,38 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-       Arrays.sort(nums1);
-       Arrays.sort(nums2);
-       int i=0;
-       int j=0;
-       ArrayList<Integer> res=new ArrayList<>();
-       while(i<nums1.length && j<nums2.length)
-       {
+     Arrays.sort(nums1);
+     Arrays.sort(nums2);
+     int i=0,j=0;
+     ArrayList<Integer> res=new ArrayList<>();
+     while(i<nums1.length && j<nums2.length)
+     {
         if(nums1[i]==nums2[j])
         {
-            if(res.isEmpty() || res.get(res.size()-1)!=nums1[i])
+            if(res.isEmpty() || res.get(res.size()-1) !=nums1[i])
             {
-           res.add(nums1[i]);
+                res.add(nums1[i]);
             }
-        
-        i++;
-        j++;
-       }
-       
+            i++;
+            j++;
+        }
         else if(nums1[i]<nums2[j])
-       {
-        i++;
-       }
-       else
-       {
-        j++;
-       }
-       }
-        int []a=new int[res.size()];
-    for(int k=0;k<res.size();k++)
-    {
-      a[k]=res.get(k); 
-    }
-    return a;
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+       
 
+     }
+
+ int[] a=new int[res.size()];
+        for(int k=0;k<res.size();k++)
+        {
+            a[k]=res.get(k);
+        }
+        return a;
     }
    
 }
