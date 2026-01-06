bc62711ef8bf9d414caf84c614253e81ec767950
jayashri-jadhav <jsahebrao329@gmail.com>
2026-01-06 07:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (69.92%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int k=n-2;
+        if(nums==null || nums.length==0)
+        {
+            return;
+        }
+        //find pos of k
+        
+       while(k>=0 && nums[k]>=nums[k+1])
+       {
+        k--;
+       }
+        if(k<0)
+        {
+            reverse(nums,0,n-1);
+            return;
+        }
+        // else replace with next greter element
+
+        for(int i=n-1;i>0;i--)
+        {
+            if(nums[i]>nums[k])
+            {
+                int temp=nums[i];
+                nums[i]=nums[k];
+                nums[k]=temp;
+                break;
+
+            }
+          
+        }
+        reverse(nums,k+1,n-1);
+    }
+    
+    private void reverse(int [] nums,int start,int end)
+    {
+        while(start<end)
+    {
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+    }
+      
+
+    }
+}
