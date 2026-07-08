85e0b47b30c3f0f5cdb1256e469dee36f908e911
jayashri-jadhav <jsahebrao329@gmail.com>
2026-07-08 15:51:38 +0530

    Time: 173 ms (5.42%) | Memory: 47.5 MB (7.56%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,36 +1,33 @@
+import java.util.*;
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        HashSet<List<Integer>> res=new HashSet<>();
-        Arrays.sort(nums);
-        if(nums==null || nums.length<4)
+        HashSet<List<Integer>> ans=new HashSet<>();
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length-3;i++)
+    {
+        for(int j=i+1;j<nums.length-2;j++)
         {
-            return new ArrayList<>();
-        }
-        for(int i=0;i<nums.length-3;i++)
-        {
-            for(int j=i+1;j<nums.length-2;j++)
+            int left=j+1;
+            int right=nums.length-1;
+            while(left<right)
             {
-                int start=j+1;
-                int end=nums.length-1;
-                while(start<end)
+                long sum= (long) nums[i]+nums[j]+nums[left]+nums[right];
+                if(sum==target)
                 {
-                long sum=(long)nums[i]+nums[j]+nums[start]+nums[end];
-                if(sum==(long)target)
-                {
-                    res.add(Arrays.asList(nums[i],nums[j],nums[start],nums[end]));
-                    start++;
-                    end--;
+                    ans.add(Arrays.asList (nums[i],nums[j],nums[left],nums[right]));
+                    left++;
+                    right--;
                 }
-                else if(sum<target)
+                else if(sum>target)
                 {
-                    start++;
+                    right--;
                 }
                 else
                 {
-                end--;
-            }}
+                    left++;
+                }
+            }
         }
-        }
- return new ArrayList<>(res);       
+    }        return new ArrayList<>(ans);
     }
 }
